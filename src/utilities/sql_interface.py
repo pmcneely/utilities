eee9f68d981d8a77a667a73771f659306735db17
Patrick McNeely <pat@hiddenstates.io>
2022-02-14 13:11:34 -0500

    Get the SQLite interface out of the table definition and creation business. Reduce the scope of this interface to data insertion/deletion

--- src/utilities/sql_interface.py
+++ src/utilities/sql_interface.py
@@ -44,96 +44,84 @@ def _retrieve_data(cur, command: str):
         raise
 
 
-def _config_tables_to_commands(tables: dict):
+# XXX
+# TODO - Remove this jazz
+# def _config_tables_to_commands(tables: dict):
 
-    # Return values
-    commands = []
+#     # Return values
+#     commands = []
 
-    for table, fields in tables.items():
-        primary_key_defined = False
-        for field_name, properties in fields.items():
-            if primary_key_defined:
-                break
-            for field_definition in properties["definition"]:
-                if "PRIMARY KEY" in field_definition:
-                    primary_key_defined = True
-                    break
+#     for table, fields in tables.items():
+#         primary_key_defined = False
+#         for field_name, properties in fields.items():
+#             if primary_key_defined:
+#                 break
+#             for field_definition in properties["definition"]:
+#                 if "PRIMARY KEY" in field_definition:
+#                     primary_key_defined = True
+#                     break
 
-        cmd = f'CREATE TABLE IF NOT EXISTS "{table}"'
-        cmd += " (id integer PRIMARY KEY, " if not primary_key_defined else " ("
+#         cmd = f'CREATE TABLE IF NOT EXISTS "{table}"'
+#         cmd += " (id integer PRIMARY KEY, " if not primary_key_defined else " ("
 
-        cmd_suffix = ""
+#         cmd_suffix = ""
 
-        for field_name in fields.keys():
-            field_properties = fields[field_name]
-            field_definition = field_properties["definition"]
-            field_t = field_definition[0].upper()
-            if field_t in SQLInterface.sqlite_types:
-                cmd += f'"{field_name}" {field_t}'
-                if len(field_definition) > 1:
-                    cmd += " " + " ".join(field_definition[1:])
-            else:
-                raise TypeError(
-                    f'Data type {field_t} (in field "{field_name}") not recognized as SQL type.'
-                    f"Available types are {SQLInterface.sqlite_types.keys()}"
-                )
-            if "foreign key" in field_properties:
-                fk_details = field_properties["foreign key"]
-                cmd_suffix += ', FOREIGN KEY ("{}") REFERENCES "{}" ("{}")'.format(
-                    field_name, fk_details[0], fk_details[1]
-                )
-            cmd += ", "
+#         for field_name in fields.keys():
+#             field_properties = fields[field_name]
+#             field_definition = field_properties["definition"]
+#             field_t = field_definition[0].upper()
+#             if field_t in SQLInterface.sqlite_types:
+#                 cmd += f'"{field_name}" {field_t}'
+#                 if len(field_definition) > 1:
+#                     cmd += " " + " ".join(field_definition[1:])
+#             else:
+#                 raise TypeError(
+#                     f'Data type {field_t} (in field "{field_name}") not recognized as SQL type.'
+#                     f"Available types are {SQLInterface.sqlite_types.keys()}"
+#                 )
+#             if "foreign key" in field_properties:
+#                 fk_details = field_properties["foreign key"]
+#                 cmd_suffix += ', FOREIGN KEY ("{}") REFERENCES "{}" ("{}")'.format(
+#                     field_name, fk_details[0], fk_details[1]
+#                 )
+#             cmd += ", "
 
-        cmd = cmd.rstrip(", ")
-        if cmd_suffix:
-            cmd += cmd_suffix
-        if primary_key_defined:
-            cmd += ") WITHOUT ROWID;"
-        else:
-            cmd += ");"
-        commands.append(cmd)
+#         cmd = cmd.rstrip(", ")
+#         if cmd_suffix:
+#             cmd += cmd_suffix
+#         if primary_key_defined:
+#             cmd += ") WITHOUT ROWID;"
+#         else:
+#             cmd += ");"
+#         commands.append(cmd)
 
-    return commands
+#     return commands
 
 
 def validate_config(config: dict):
     try:
-        assert {"path", "tables"}.issubset(
+        assert {"project dir", "db config", "db file"}.issubset(
             config.keys()
         ), "Invalid configuration for SQLLite interface"
-        tables = config["tables"]
-        assert isinstance(
-            tables, dict
-        ), "Table creation definition must be a dictionary. See documentation"
-        for fields in tables.values():
-            assert isinstance(
-                fields, dict
-            ), "Field definitions for tables must be a list of dictionaries. See documentation"
-    except Exception:
+        project_dir = os.path.abspath(os.path.expanduser(config["project dir"]))
+        db_config_file = config["db config"]
+        config_path = os.path.join(project_dir, db_config_file)
+        assert os.path.isfile(
+            config_path
+        ), f"No database defnition file found at {config_path}"
+    except AssertionError:
         raise ValueError("Invalid configuration. See stack trace for information")
 
 
 class SQLInterface:
-
-    sqlite_types = {
-        "NULL": "",
-        "INTEGER": int,
-        "REAL": float,
-        "TEXT": str,
-        "BLOB": bytes,
-    }
-
     def __init__(self, config: dict = None, log_name: str = None):
         """
         'config' should include the following information:
-        'path': Base directory for where data should be stored
-              For SQLite databases, this will be the root folder
-              which contains the database and the 'DB' folder
-              that contains linked information.
-        'project': Project name for the database to inherit. Must be provided
-                   in the configuration. Will yield the file '<project>.db'
-        'tables': Definition of tables as key-value pairs, e.g.
-              {'<table_name>': {'<field_name>', 'type: [<sqlite type> OR <ptr pointer/path>}']}
+            'path': Base directory for where data should be stored
+                For SQLite databases, this will be the root folder
+                which contains the database and the 'DB' folder
+                that contains linked information.
+            'db file': Pre-created database file (SQLite-only!) at `path`
         """
 
         assert (
@@ -167,14 +155,27 @@ class SQLInterface:
         validate_config(config)
         self.config = config
 
-        db_name = "DB" if "project" not in config else (config["project"] + "_DB")
-        base_path = os.path.abspath(os.path.expanduser(config["path"]))
+        db_name = config["db file"]
+        base_path = os.path.abspath(os.path.expanduser(config["project dir"]))
         self.db_path = base_path
-        self.db = base_path + db_name + ".db"
-        if self.logger:
-            self.logger.debug(f"Attempting to connect to database at path {self.db}")
+        self.db = os.path.join(base_path, db_name)
+        self.logger.debug(f"Attempting to connect to database at path {self.db}")
+        db_exists = True
+        if not os.path.isfile(self.db):
+            self.logger.info(f" --- Did not find a database file at {self.db}")
+            self.logger.info(" --- Attempting to create the database")
+            db_exists = False
         self.conn = sqlite3.connect(self.db)
         self.cur = self.conn.cursor()
+        if not db_exists:
+            try:
+                config_path = os.path.join(base_path, config["db config"])
+                with open(config_path, 'r') as db_config:
+                    sql_string = db_config.read()
+                    self.cur.executescript(sql_string)
+                db_config.close()
+            except:
+                raise
         self.meta_info = {"tables": {}}
 
     def activate_logging(self):
@@ -187,29 +188,29 @@ class SQLInterface:
             self.logger.deactivate()
             self.logger.info("Deactivated logging facility")
 
-    def create_tables(self):
+    # XXX Let SQLite handle creation/removal of tables.
+    # TODO: Change DB configuration file (`.yaml`) to SQL definition (`.sql`)
+    # def create_database(self):
 
-        if self.logger:
-            self.logger.debug(f"Creating tables (from internal configuration)")
-        tables = self.config["tables"]
-        table_creation_commands = _config_tables_to_commands(tables)
-        if self.logger:
-            self.logger.debug(f"----> Executing table creation commands <----")
-            for cmd in table_creation_commands:
-                self.logger.debug(f"\t- {cmd}")
-        for cmd in table_creation_commands:
-            _execute_command(self.cur, cmd)
-        if self.logger:
-            self.logger.debug(
-                f"Processed tables (type {type(tables)})\n{'*'*80}\n{tables}\n{'*'*80}"
-            )
-        self.retrieve_metadata()
+    #     if self.logger:
+    #         self.logger.debug(f"Creating tables (from internal configuration)")
+    #     tables = self.config["tables"]
+    #     table_creation_commands = _config_tables_to_commands(tables)
+    #     if self.logger:
+    #         self.logger.debug(f"----> Executing table creation commands <----")
+    #         for cmd in table_creation_commands:
+    #             self.logger.debug(f"\t- {cmd}")
+    #     for cmd in table_creation_commands:
+    #         _execute_command(self.cur, cmd)
+    #     if self.logger:
+    #         self.logger.debug(
+    #             f"Processed tables (type {type(tables)})\n{'*'*80}\n{tables}\n{'*'*80}"
+    #         )
+    #     self.retrieve_metadata()
 
     def get_tables(self):
         table_command = "SELECT name FROM sqlite_master WHERE type='table';"
         tables = _retrieve_data(self.cur, table_command)
-        # for table in tables:
-        if self.logger:
         self.logger.debug([table_name[0] for table_name in tables])
         return [table_name[0] for table_name in tables]
 
@@ -231,20 +232,19 @@ class SQLInterface:
         )
 
     def table_to_tuples_for_entry(self, table, fields):
-        if self.logger:
-            self.logger.debug(
-                f"Table conversion: Got table {table} and fields {fields}"
-            )
-            self.logger.debug(f"---> Table conversion is {string_to_camel_case(table)}")
-            self.logger.debug(
-                f"Field conversion is {[(field[1], SQLInterface.sqlite_types[field[2].upper()]) for field in fields if field[1] != 'id']}"
-            )
+        self.logger.debug(
+            f"Table conversion: Got table {table} and fields {fields}"
+        )
+        self.logger.debug(f"---> Table conversion is {string_to_camel_case(table)}")
+        self.logger.debug(
+            f"Field conversion is {[(field[1], field[2].upper()) for field in fields if field[1] != 'id']}"
+        )
         # NB: This ignores the 'id' field on the assumption that it is an AUTOINCREMENTing field handled by the database
         return NamedTuple(
             string_to_camel_case(table),
             [
                 # *Pylance isn't perfect, and gets confused with this comprehension. Hence, `type: ignore`*
-                (field[1], SQLInterface.sqlite_types[field[2].upper()])  # type: ignore
+                (field[1], field[2].upper())  # type: ignore
                 for field in fields
                 if field[1] != "id"
             ],
@@ -262,11 +262,10 @@ class SQLInterface:
             field_names = self.meta_info["tables"][table].__annotations__.keys()
             values = []
 
-            if self.logger:
-                self.logger.debug(f"Found field name information: {field_names}")
-                self.logger.debug("Received data:")
-                for item in data_list:
-                    self.logger.debug(f"\t- {item}")
+            self.logger.debug(f"Found field name information: {field_names}")
+            self.logger.debug("Received data:")
+            for item in data_list:
+                self.logger.debug(f"\t- {item}")
             for item in data_list:
                 values.append(str(tuple(item)))
             insert_command = 'INSERT INTO "{}" ({}) VALUES {};'.format(
@@ -274,9 +273,8 @@ class SQLInterface:
                 ",".join(field_names),
                 ",".join(values),
             )
-            if self.logger:
-                self.logger.debug(f"Issuing insertion command")
-                self.logger.debug(insert_command)
+            self.logger.debug(f"Issuing insertion command")
+            self.logger.debug(insert_command)
             try:
                 _execute_command(self.cur, insert_command)
             except Exception as e:
@@ -300,17 +298,16 @@ class SQLInterface:
                 field_list = [
                     f'"{f}"' for idx, f in enumerate(fields) if item[idx] is not None
                 ]
-                if self.logger:
-                    self.logger.debug(f"Found a deletion field list {field_list}")
-                    self.logger.debug(
-                        "Running deletion command {}".format(
-                            deletion_cmd.format(
-                                table,
-                                ",".join(field_list),
-                                ",".join([f'"{i}"' for i in item if i is not None]),
-                            ),
-                        )
+                self.logger.debug(f"Found a deletion field list {field_list}")
+                self.logger.debug(
+                    "Running deletion command {}".format(
+                        deletion_cmd.format(
+                            table,
+                            ",".join(field_list),
+                            ",".join([f'"{i}"' for i in item if i is not None]),
+                        ),
                     )
+                )
                 _execute_command(
                     self.cur,
                     deletion_cmd.format(
@@ -328,8 +325,7 @@ class SQLInterface:
          so I leave simplicity/compexity of implementation to the application.
          This package only handles the cursor/connection itself)
         """
-        if self.logger:
-            self.logger.debug(f"Received data query \n\t---> {query}")
+        self.logger.debug(f"Received data query \n\t---> {query}")
         try:
             data = _retrieve_data(self.cur, query)
             return data
