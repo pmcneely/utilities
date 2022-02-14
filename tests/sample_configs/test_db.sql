455030a3f2671bb5183f39af9fdba00571f154e8
Patrick McNeely <pat@hiddenstates.io>
2022-02-14 15:31:18 -0500

    Add breaking example of a field name crashing the interface

--- tests/sample_configs/test_db.sql
+++ tests/sample_configs/test_db.sql
@@ -2,8 +2,8 @@ PRAGMA foreign_keys = ON;
 create TABLE IF NOT EXISTS "apple pie" (alice INTEGER, bob TEXT);
 create TABLE IF NOT EXISTS "bananas foster" (claire REAL, dave BLOB, erin TEXT PRIMARY KEY);
 create TABLE IF NOT EXISTS "banana details" (
-    fred TEXT NOT NULL,
+    "fred rumors" TEXT NOT NULL,
     gina REAL,
     herb INTEGER,
-    FOREIGN KEY (fred) REFERENCES "bananas foster" (erin)
+    FOREIGN KEY ("fred rumors") REFERENCES "bananas foster" (erin)
 );
