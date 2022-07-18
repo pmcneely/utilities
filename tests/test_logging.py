be8aa907a94933cd40a74693f22e1f92aee6df8e
Pat McNeely <pmcneely2@gmail.com>
2022-07-18 11:57:43 -0400

    update logger tests, function name inspection works fine when at a non-trivial code depth.

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -26,3 +26,21 @@ restored_logger.info(f"Working again! (From a new logger")
 standard_logger.debug("The original logger is now debugging")
 silent_logger.debug("In space, no one can hear you scream")
 restored_logger.debug("Restored logger, now debugging!")
+
+logger_list = [standard_logger, silent_logger, restored_logger]
+
+def func_to_test_info_level(msg: str, loggers: list[logging.Logger]) -> None:
+    if type(loggers) is logging.Logger:
+        loggers = [loggers]
+    else:
+        assert (
+            type(loggers) is list
+        ), "Argument `loggers` must be either logging.Logger or list of [logging.Logger]"
+        assert [type(l) is logging.Logger for l in loggers], \
+            "All items in list `loggers` must be of type logging.Logger"
+    for l in loggers:
+        print(f"{'-'*20}{'Info Testing: Testing logger ' + l.name:^40}{'-'*20}")
+        l.info(f"{l.name}: {msg}")
+        print(f"{'-'*20}{l.name + ': Test complete':^40}{'-'*20}")
+
+func_to_test_info_level("Testing function name inspection!", logger_list)
